6d02436bf31ebed231edc74175d7f3552838c157
ohanke <steamoha@wp.pl>
2022-04-16 11:11:32 +0200

    multiple GreetingService implementations with @Qualifier's

--- src/main/java/com/oscarhanke/sfidi/controllers/PropertyInjectedController.java
+++ src/main/java/com/oscarhanke/sfidi/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package com.oscarhanke.sfidi.controllers;
 
 import com.oscarhanke.sfidi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyInjectedGreetingService")
     @Autowired
     public GreetingService greetingService;
     public String getGreeting(){
