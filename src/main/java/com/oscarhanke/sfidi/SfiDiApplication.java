f78f393caae5e433456a4e50ee5b39e99e5b056a
ohanke <steamoha@wp.pl>
2022-04-24 13:54:55 +0200

    bean scope example

--- src/main/java/com/oscarhanke/sfidi/SfiDiApplication.java
+++ src/main/java/com/oscarhanke/sfidi/SfiDiApplication.java
@@ -6,6 +6,8 @@ import com.oscarhanke.sfidi.controllers.MyController;
 import com.oscarhanke.sfidi.controllers.PetController;
 import com.oscarhanke.sfidi.controllers.PropertyInjectedController;
 import com.oscarhanke.sfidi.controllers.SetterInjectedController;
+import com.oscarhanke.sfidi.services.PrototypeBean;
+import com.oscarhanke.sfidi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -41,6 +43,17 @@ public class SfiDiApplication {
 		ConstructorInjectedController constructorInjectedController =
 				(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("------------ Beans Scopes ----------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
---
+++ src/main/java/com/oscarhanke/sfidi/services/SingletonBean.java
@@ -0,0 +1,14 @@
+package com.oscarhanke.sfidi.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonBean {
+    public SingletonBean() {
+        System.out.println("Creating a singleton bean!!!");
+    }
+
+    public String getMyScope(){
+        return "I'm a Singleton";
+    }
+}
