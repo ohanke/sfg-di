6d55ccffbfdaca5fcbc570dc456198c431b15be0
ohanke <steamoha@wp.pl>
2022-04-24 12:09:48 +0200

    primary beans and profiles

--- src/main/java/com/oscarhanke/sfidi/config/GreetingServiceConfig.java
+++ src/main/java/com/oscarhanke/sfidi/config/GreetingServiceConfig.java
@@ -1,13 +1,36 @@
 package com.oscarhanke.sfidi.config;
 
 import com.oscarhanke.sfidi.services.ConstructorGreetingService;
+import com.oscarhanke.sfidi.services.I18nEnglishGreetingService;
+import com.oscarhanke.sfidi.services.I18nSpanishGreetingService;
+import com.oscarhanke.sfidi.services.PrimaryGreetingService;
 import com.oscarhanke.sfidi.services.PropertyInjectedGreetingService;
 import com.oscarhanke.sfidi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
