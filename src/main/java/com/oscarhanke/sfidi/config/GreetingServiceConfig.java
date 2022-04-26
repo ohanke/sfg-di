d31cb198a14a608cbdb3c9a1cdd1108f26d30c20
ohanke <steamoha@wp.pl>
2022-04-26 09:17:15 +0200

    changed properties to yml files

--- src/main/java/com/oscarhanke/sfidi/config/GreetingServiceConfig.java
+++ src/main/java/com/oscarhanke/sfidi/config/GreetingServiceConfig.java
@@ -16,9 +16,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
